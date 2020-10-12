3f8d9d9f793af7a09bb48d2c9e2ded6de6b58da9
Abby Husske <amhusske1@gmail.com>
2020-10-12 16:49:23 -0500

    add flower

--- FlowerShop/src/model/Flower.java
+++ FlowerShop/src/model/Flower.java
@@ -1,5 +1,117 @@
 package model;
 
+import java.util.List;
+
 public class Flower {
+  private int id;
+  private String flowerName;
+  private String seasonality;
+  private String sunNeeds;
+  private String soilNeeds;
+  private List<FlowerColor> colorList;
+  
+  
+  
+public Flower() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+
+
+public Flower(int id, String flowerName, String seasonality, String sunNeeds, String soilNeeds, List<FlowerColor> colorList) {
+	super();
+	this.id = id;
+	this.flowerName = flowerName;
+	this.seasonality = seasonality;
+	this.sunNeeds = sunNeeds;
+	this.soilNeeds = soilNeeds;
+	this.colorList = colorList;
+}
+
+
+
+public Flower(String flowerName, String seasonality, String sunNeeds, String soilNeeds, List<FlowerColor> colorList) {
+	super();
+	this.flowerName = flowerName;
+	this.seasonality = seasonality;
+	this.sunNeeds = sunNeeds;
+	this.soilNeeds = soilNeeds;
+	this.colorList = colorList;
+}
+
+
+
+public int getId() {
+	return id;
+}
+
+
+
+public void setId(int id) {
+	this.id = id;
+}
+
+
+
+public String getFlowerName() {
+	return flowerName;
+}
+
+
+
+public void setFlowerName(String flowerName) {
+	this.flowerName = flowerName;
+}
+
+
+
+public String getSeasonality() {
+	return seasonality;
+}
+
+
+
+public void setSeasonality(String seasonality) {
+	this.seasonality = seasonality;
+}
+
+
+
+public String getSunNeeds() {
+	return sunNeeds;
+}
+
+
+
+public void setSunNeeds(String sunNeeds) {
+	this.sunNeeds = sunNeeds;
+}
+
+
+
+public String getSoilNeeds() {
+	return soilNeeds;
+}
+
+
+
+public void setSoilNeeds(String soilNeeds) {
+	this.soilNeeds = soilNeeds;
+}
+
+
+
+public List<FlowerColor> getColorList() {
+	return colorList;
+}
+
+
+
+public void setColorList(List<FlowerColor> colorList) {
+	this.colorList = colorList;
+}
+
+
 
 }
---
+++ FlowerShop/src/model/FlowerColor.java
@@ -0,0 +1,23 @@
+package model;
+
+public class FlowerColor {
+	private String color;
+
+	public FlowerColor() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FlowerColor(String color) {
+		super();
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
